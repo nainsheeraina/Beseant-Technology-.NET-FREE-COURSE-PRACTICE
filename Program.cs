e9b928450c45e15cf39012fa34892bb355bb9e49
Nainshee Raina <nainsheeraina@gmail.com>
2023-01-12 20:16:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Beseant");
+            //Console.WriteLine(".NET");
+            //Console.ReadKey();
+           // String name;
+           // Console.WriteLine("Enter Your Name");
+           //name= Console.ReadLine();
+           //Console.WriteLine("Enter Your Name" +name);
+           int a, b, sum;
+            Console.WriteLine("Enter a");
+            a=Convert.ToInt32(Console.ReadLine()) ;
+            Console.WriteLine("Enter B");
+            b=Convert.ToInt32(Console.ReadLine());
+            sum=a+b;
+            Console.WriteLine(sum);
+
+           Console.ReadKey();
+        }
+    }
+}
